f0d68d069570b04e5313a92689253774a117faef
Irving Suarez <irfdev@IrfAsus.localdomain>
2023-06-25 17:42:36 -0600

    Fixing 1st ex

--- 27_jun/1st.py
+++ 27_jun/1st.py
@@ -4,14 +4,13 @@ is_user_finished = False
 def ask_for_name():
     new_name = input("Please write a name: ")
 
-    tupple_of_names.insert(new_name)
+    tupple_of_names.pop(new_name)
 
 def ask_to_continue():
-    is_first_iteration = len(tupple_of_names) > 1 
-    
-    should_we_continue = is_first_iteration and input("Do you want to input more names? True/False") or "True"
+    is_first_iteration = len(tupple_of_names) > 0 
+    should_we_continue = input("Do you want to continue? True/False") if is_first_iteration == True else "True"
 
-    lowercase_continuing_boolean = "{should_we_continue}".lower()
+    lowercase_continuing_boolean = f'{should_we_continue}'.lower()
 
     if lowercase_continuing_boolean == "true":
         ask_for_name()
