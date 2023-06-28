b71ecdf7afd120fa0901206602f9e398395a7ca7
Irving <irving.dev@icloud.com>
2023-06-27 19:04:49 -0600

    adding first exercises

---
+++ n.py
@@ -0,0 +1,5 @@
+int_num = int(input('Ingresa número entero: '))
+
+suma = int_num + (int_num*int_num) + (int_num*int_num*int_num)
+
+print(f'La suma de {int_num} mas su cuadrado y su cubo es: {int(suma)}')
---
+++ sing_vibora.py
@@ -0,0 +1,3 @@
+A_LA_VIBORA_LYRICS = 'A la víbora, víbora\nDe la mar, de la mar\nPor aquí pueden pasar\nLos de adelante corren mucho\nY los de atrás se quedarán\n\nA la víbora, víbora\nDe la mar, de la mar\nPor aquí pueden pasar \nLos de adelante corren mucho \nY los de atrás se quedarán \nTras, tras, tras'
+
+print(A_LA_VIBORA_LYRICS)
